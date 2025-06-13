7f3684c9a0203d9e8af3e4bd89f05d8e2aede7bc
lilwoutie <wout.welvaarts@gmail.com>
2025-06-14 00:25:05 +0200

    small bugfix

---
+++ supplier-service-1/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, description, price, available, quantity) VALUES
+('Margherita Pizza', 'Classic pizza with tomato, mozzarella, and basil', 10.00, true, 20),
+('Pepperoni Pizza', 'Pizza with tomato, mozzarella, and pepperoni', 11.50, true, 20),
+('Caesar Salad', 'Romaine lettuce with Caesar dressing and croutons', 7.00, true, 20),
+('Tomato Soup', 'Creamy tomato soup with basil', 5.00, true, 30),
+('Garlic Bread', 'Toasted bread with garlic and herbs', 3.00, true, 45);
